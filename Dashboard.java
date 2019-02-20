76bc3a03ca2ac2853ba4b153246b704d5b00f322
HuDeeni97 <46978275+HuDeeni97@users.noreply.github.com>
2019-02-20 16:38:13 -0500

    Dashboard shell
    adding one more shell

---
+++ Dashboard.java
@@ -0,0 +1,23 @@
+// ***************************************************************
+// Names: Nathaniel Deen, Bailey Roberts, Jayson Tinsley, Liam Kehoe, Matthieu Privat
+// Last Updated: 2/20/2019
+// Version: 0.01
+//
+// SE 300, Section 1, Final Group Project Run File
+//
+// Class: Courses_Taken extends Pane
+//
+// Attributes: 
+//
+// Methods: 
+// ***************************************************************
+
+import javafx.scene.layout.Pane;
+
+public class Dashboard extends Pane {
+
+	//constructor
+	public Dashboard(){
+
+	}
+}//end Dashboard
