6dabb37c165ff17976c4d7b0cc28bb77ab0b35b6
HuDeeni97 <46978275+HuDeeni97@users.noreply.github.com>
2019-02-20 16:36:55 -0500

    File Shells
    This is a batch of the file shells to be filled in

---
+++ Login_GUI.java
@@ -0,0 +1,53 @@
+// ***************************************************************
+// Names: Nathaniel Deen, Bailey Roberts, Jayson Tinsley, Liam Kehoe, Matthieu Privat
+// Last Updated: 2/20/2019
+// Version: 0.01
+//
+// SE 300, Section 1, Final Group Project Run File
+//
+// Class: Login_GUI extends Pane
+//
+// Attributes: 
+//
+// Methods: 
+//
+// Description:
+// 
+// IF LOGGING IN TO CREATED PROFILE:
+// During login, this class will have methods to check for available usernames. If
+// the input username matches any available username, the methods will then check
+// the input password to see if it matches the password linked with the found
+// username.
+// Login GUI will be the main interface after the SchedulER launcher. After login
+// is complete, this will then open up the main dashboard where users can create
+// schedules and change user settings.
+//
+// IF CREATING AN ACCOUNT:
+// If the user picks the "Create Account" option from the launcher page, they will
+// enter a username and password. The username will be checked against existing
+// usernames so that two usernames will not be identical.
+// The password would then be checked against password standards for a practical
+// safe password. The user would the be shown the "class input" page to continue
+// creating their profile.
+// ***************************************************************
+
+public class Login_GUI {
+
+	public Settings_Page m_Settings_Page;
+
+	public Login_GUI(){
+
+	}
+
+	private void CheckLogin(){
+
+	}
+
+	private void CreateAccount(){
+
+	}
+
+	public char[] getUser(){
+		return null;
+	}
+}//end Login_GUI
---
+++ Settings_Page.java
@@ -0,0 +1,62 @@
+// ***************************************************************
+// Names: Nathaniel Deen, Bailey Roberts, Jayson Tinsley, Liam Kehoe, Matthieu Privat
+// Last Updated: 2/20/2019
+// Version: 0.01
+//
+// SE 300, Section 1, Final Group Project Run File
+//
+// Class: SchedulERlauncher extends Application
+//
+// Attributes: 
+//
+// Methods: 
+//
+// Description:
+// An area where users input classes that they have taken previously. Once the
+// user is done adding classes that they have previously taken, there will be a
+// save button that the user must click before leaving that page.
+// Save button then saves user class information directly to an excel file where
+// it can then be later called for other functions.
+// ***************************************************************
+
+import javafx.scene.layout.Pane;
+
+public class Settings_Page extends Pane {
+
+	//Initializes Attributes
+	private char[] UserProfile;
+
+	//screen constructor
+	public Settings_Page(){
+
+	}
+
+	//methods section
+	protected void ChangeDegree(){
+
+	}
+
+	private void ChangePassword(){
+
+	}
+
+	public void main(){
+
+	}
+
+	public void Read_Data(){
+
+	}
+
+	protected void UpdateClasses(){
+
+	}
+
+	protected int UpdateCredits(){
+		return 0;
+	}
+
+	public void Write_Data(){
+
+	}
+}//end Settings_Page
